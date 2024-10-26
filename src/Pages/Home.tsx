aa356e42c64051634f739c4c1a0dce201351dd2a
Akash Nagaonkar <akash.nagaonkar@infosys.com>
2024-10-26 22:08:25 +0530

    Add static Navbar and Siderbar

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,8 +1,17 @@
+import Navbar from "../Components/Navbar";
+import Sidebar from "../Components/Sidebar";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="max-h-screen overflow-hidden">
+      <div className="h-[6vh]">
+        <Navbar />
+      </div>
+      <div className="flex h-[94vh]">
+        <Sidebar />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
